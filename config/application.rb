dc587d29a835ac80cc7c6032a4d09d3f37f9ae54
tsubasa1122 <nakanot@open8.com>
2020-01-12 22:32:56 +0900

    初期設定追加

--- config/application.rb
+++ config/application.rb
@@ -28,8 +28,11 @@ module RspecSample
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
 
-    # Don't generate system test files.
+    # Don't generate system test files. 
     config.generators.system_tests = nil
   end
 end
