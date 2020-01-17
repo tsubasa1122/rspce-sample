79eb39796242a334ca2bffbaccd01c630c4b5313
tsubasa1122 <nakanot@open8.com>
2020-01-17 21:52:07 +0900

    正常な動作確認

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe FoodEnquete, type: :model do
-  describe '正常系の機能' do
+  describe '正常系機能' do
     context '回答する' do
-      it '正しく登録できること　料理: 焼きそば food_id:2, 満足度:良い score: 3, 希望するプレゼント: ビール飲み放題 present_id: 1' do
+      it '正しく登録すること　料理:やきそば food_id: 2, 満足度:良い score: 3, 希望するプレゼント:ビール飲み放題 present_id: 1' do
         enquete = FoodEnquete.new(
           name: '田中 太郎',
           mail: 'taro.tanaka@example.com',
@@ -16,14 +16,15 @@ RSpec.describe FoodEnquete, type: :model do
 
         expect(enquete).to be_valid
         enquete.save
+        answered_enquete = FoodEnquete.find(1);
 
-        answerd_enquete = FoodEnquete.find(1)
-        expect(answerd_enquete.name).to eq('田中 太郎')
-        expect(answerd_enquete.mail).to eq('taro.tanaka@example.com')
-        expect(answerd_enquete.age).to eq(25)
-        expect(answerd_enquete.food_id).to eq(2)
-        expect(answerd_enquete.request).to eq('おいしかったです。')
-        expect(answerd_enquete.present_id).to eq(1)
+        expect(answered_enquete.name).to eq('田中 太郎')
+        expect(answered_enquete.mail).to eq('taro.tanaka@example.com')
+        expect(answered_enquete.age).to eq(25)
+        expect(answered_enquete.food_id).to eq(2)
+        expect(answered_enquete.score).to eq(3)
+        expect(answered_enquete.request).to eq('おいしかったです。')
+        expect(answered_enquete.present_id).to eq(1)
       end
     end
   end
