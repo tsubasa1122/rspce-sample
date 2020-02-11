4768c3a08c4fe8df38a5a4225a2739a1a2e8cfd6
tsubasa1122 <nakanot@open8.com>
2020-02-11 20:56:40 +0900

    一部をfactorybotに切り替える

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -4,15 +4,7 @@ RSpec.describe FoodEnquete, type: :model do
   describe '正常系機能' do
     context '回答する' do
       it '正しく登録すること　料理:やきそば food_id: 2, 満足度:良い score: 3, 希望するプレゼント:ビール飲み放題 present_id: 1' do
-        enquete = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1
-        )
+        enquete = FactoryBot.build(:food_enquete)
 
         expect(enquete).to be_valid
         enquete.save
@@ -116,26 +108,10 @@ RSpec.describe FoodEnquete, type: :model do
   describe 'アンケートの回答時の条件' do
     context 'メールアドレスを確認すること' do
       it '同じメールアドレスで再び回答できないこと' do
-        enquete_tanaka = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 1
-        )
-        enquete_tanaka.save
+        FactoryBot.create(:food_enquete)
 
-        re_enquete_tanaka = FoodEnquete.new(
-          name: '田中 太郎',
-          mail: 'taro.tanaka@example.com',
-          age: 25,
-          food_id: 2,
-          score: 3,
-          request: 'おいしかったです。',
-          present_id: 0
-        )
+        re_enquete_tanaka = FactoryBot.build(:food_enquete, food_id: 0, score: 1, present_id: 0, request: 'スープがぬるかった')
+
         expect(re_enquete_tanaka).not_to be_valid
 
         expect(re_enquete_tanaka.errors[:mail]).to include(I18n.t('errors.messages.taken'))
@@ -145,16 +121,7 @@ RSpec.describe FoodEnquete, type: :model do
     end
 
     it '異なるメールアドレスで回答できること' do
-      enquete_tanaka = FoodEnquete.new(
-        name: '田中 太郎',
-        mail: 'taro.tanaka@example.com',
-        age: 25,
-        food_id: 2,
-        score: 3,
-        request: 'おいしかったです。',
-        present_id: 1
-      )
-      enquete_tanaka.save
+      FactoryBot.create(:food_enquete)
 
       enquete_yamada = FoodEnquete.new(
         name: '山田 次郎',
