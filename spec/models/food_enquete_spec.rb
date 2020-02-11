41cb34310b9d1b9cb465b5fdb71e15af09fbdf79
tsubasa1122 <nakanot@open8.com>
2020-02-11 20:41:38 +0900

    テスト追加

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -57,4 +57,80 @@ RSpec.describe FoodEnquete, type: :model do
       end
     end
   end
+
+  describe 'アンケート回答時の条件' do
+    context '年齢を確認すること' do
+      it '未成年はビール飲み放題を選択できないこと' do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 19,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+  
+        expect(enquete_sato).not_to be_valid
+        expect(enquete_sato.errors[:present_id]).to include(I18n.t('activerecord.errors.models.food_enquete.attributes.present_id.cannot_present_to_minor'))
+      end
+
+      it '成人はビール飲み放題を選択できないこと' do
+        enquete_sato = FoodEnquete.new(
+          name: '佐藤 仁美',
+          mail: 'hitomi.sato@example.com',
+          age: 20,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        expect(enquete_sato).to be_valid
+      end
+    end
+  end
+
+  describe '#adult?' do
+    it '20歳未満は成人ではないこと' do
+      foodEnquete = FoodEnquete.new
+      expect(foodEnquete.send(:adult?, 19)).to be_falsey
+    end
+
+    it '20歳以上は成人であること' do
+      foodEnquete = FoodEnquete.new
+      expect(foodEnquete.send(:adult?, 20)).to be_truthy
+    end
+  end
+
+  describe 'アンケートの回答時の条件' do
+    context 'メールアドレスを確認すること' do
+      it '同じメールアドレスで再び回答できないこと' do
+        enquete_tanaka = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 1
+        )
+        enquete_tanaka.save
+
+        re_enquete_tanaka = FoodEnquete.new(
+          name: '田中 太郎',
+          mail: 'taro.tanaka@example.com',
+          age: 25,
+          food_id: 2,
+          score: 3,
+          request: 'おいしかったです。',
+          present_id: 0
+        )
+        expect(re_enquete_tanaka).not_to be_valid
+
+        expect(re_enquete_tanaka.errors[:mail]).to include(I18n.t('errors.messages.taken'))
+        expect(re_enquete_tanaka.save).to be_falsey
+        expect(FoodEnquete.all.size).to eq 1
+      end
+    end
+  end
 end
