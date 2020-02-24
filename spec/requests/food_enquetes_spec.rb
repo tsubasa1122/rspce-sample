13277740c80e05e56819737dd90de820bb8c0a68
tsubasa1122 <nakanot@open8.com>
2020-02-24 22:32:22 +0900

    [fix]コントローラーのテストコード

--- spec/requests/food_enquetes_spec.rb
+++ spec/requests/food_enquetes_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe "FoodEnquetes", type: :request do
         
         post '/food_enquetes', params: { food_enquete: FactoryBot.attributes_for(:food_enquete_tanaka) }
         follow_redirect!
-        expect(response.body).to include'お食事に関するアンケートを送信しました'
+        expect(response.body).to include'ご回答ありがとうございました'
       end
     end
   end
@@ -22,7 +22,7 @@ RSpec.describe "FoodEnquetes", type: :request do
 
         post "/food_enquetes", params: { food_enquete: { name: ''} }
 
-        expect(response.body).not_to include 'お食事に関するアンケートを送信しました'
+        expect(response.body).not_to include 'ご回答ありがとうございました'
       end
     end
   end
