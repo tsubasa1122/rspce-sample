d6f9fd7604ab4ff5c26b41047281cc0dd2a43735
tsubasa1122 <nakanot@open8.com>
2020-02-24 22:24:54 +0900

    [fix]コードを少し修正

--- app/models/concerns/common_module.rb
+++ app/models/concerns/common_module.rb
@@ -1,16 +1,15 @@
 module CommonModule
   extend ActiveSupport::Concern
 
-  TAX = 1.08        # 本体価格 + 消費税率
+  TAX = 1.1        # 本体価格 + 消費税率
   SCORE_BAD = 1     # 満足度:悪い
   SCORE_NORMAL = 2  # 満足度:普通
   SCORE_GOOD = 3    # 満足度:良い
 
   included do
-    
     validates :name, presence: true, length: { maximum: 16 }
-    validates :mail, presence: true, length: { maximum: 64 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: { case_sensitive: false}
-    validates :age, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+    validates :mail, presence: true, length: { maximum: 64 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :age, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
     validates :score, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
     validates :request, length: { maximum: 150 }
   end
